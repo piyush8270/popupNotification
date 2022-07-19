5bae0e546e9593a7ba34e61c4cf83457e9ba8f39
piyush8270 <piyushpadhy4444@gmail.com>
2022-07-19 14:29:18 +0530

    notification

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//import { MatFormFieldModule } from '@angular/material/form-field';
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,7 +13,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    //MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
